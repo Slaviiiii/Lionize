d8656223640c4f06971a0e5adcb875451666ff8f
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-01-09 15:41:26 +0200

    feat: add basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center p-5 shadow-2xl mt-10 dark:shadow-gray-50 shadow-black">
+      &copy; 2023 All rights reserved
+    </footer>
+  );
+};
+
+export default Footer;
